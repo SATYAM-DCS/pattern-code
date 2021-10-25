179df23a1242819caac1d37cb5e0665e6bb735fe
SATYAM-DCS <93065636+SATYAM-DCS@users.noreply.github.com>
2021-10-25 10:31:26 +0530

    Add files via upload

---
+++ Patterns.java
@@ -0,0 +1,26 @@
+package basics;
+import java.util.Scanner;
+
+public class Patterns {
+
+	public static void main(String[] args) {
+	    Scanner s = new Scanner(System.in);
+	        int n = s.nextInt();
+	        int i = 1;
+	        while(i<=n)
+	        {
+	           int j = 1;
+	            while(j<=n)
+	            {
+	                System.out.print("4");
+	                 j = j+1;
+	            }
+	        }
+	          System.out.println();
+	        i = i+1;	
+		}
+
+
+	}
+
+
