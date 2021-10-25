630da47009f8265e4f4b66d32b9d16329f43448d
SATYAM-DCS <93065636+SATYAM-DCS@users.noreply.github.com>
2021-10-25 10:26:37 +0530

    Add files via upload

---
+++ Patterns8.java
@@ -0,0 +1,29 @@
+package basics;
+import java.util.Scanner;
+
+public class Patterns8 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int i = 1;
+		while(i<=n)
+		{
+			int space = 1;
+			while(space <= n - i)
+			{ 
+				System.out.print(' ');
+				space = space+1;
+			}
+			int star = 1;
+			while(star <= i)
+			{ 
+				System.out.print('*');
+				star = star+1;
+			}
+			System.out.println();
+			i = i + 1;
+		}
+
+	}
+}
